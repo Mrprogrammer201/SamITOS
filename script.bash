996e8f6c12b890ae717888759b544c5d23b867f2
Samvel Mirzoyants <mirzoyantssamvel@gmail.com>
2025-04-13 14:03:23 +0500

    Refactoring: new project structure, added modules FS, PM, boot, utils

--- script.bash
+++ script.bash
@@ -1,21 +1,6 @@
-#!/bin/bash
-
-# Цвета для вывода
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
-echo -e "${GREEN}[*] Обновление репозитория SamITOS_SIM...${NC}"
-
-# Определяем текущую ветку
-BRANCH=$(git symbolic-ref --short HEAD)
-
-# Добавляем все файлы
+git init
+git remote add origin https://github.com/Programmer201/SamITOS.git
+git checkout -b main
 git add .
-
-# Коммитим с фиксированным сообщением
 git commit -m "Refactoring: new project structure, added modules FS, PM, boot, utils"
-
-# Отправляем изменения в текущую ветку
-git push origin "$BRANCH"
-
-echo -e "${GREEN}[✓] Репозиторий обновлён успешно в ветке '$BRANCH'.${NC}"
+git push -u origin main
