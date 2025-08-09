4ecfa1f7e6d088ffb72e5c93a076f1a5eaaa0983
Samvel Mirzoyants <158126630+Mrprogrammer201@users.noreply.github.com>
2025-08-09 11:29:13 +0500

    Update CommandHandler.c

--- core/CommandHandler.c
+++ core/CommandHandler.c
@@ -4,164 +4,174 @@
 #include "PM/PM.h"
 #include "Toolkit/Toolkit.h"
 #include "Utils/Nicopat.h"
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+#define CMD_BUFFER 512
+
+typedef void (*command_func_t)(char *args);
+
+typedef struct {
+const char *name;
+command_func_t handler;
+const char *description;
+} Command;
+
+// ==== Command handlers ====
+void cmd_ls(char *args) { system("ls UserFiles"); }
+void cmd_touch(char *args) { cr_fl(args); }
+void cmd_rm(char *args) { dl_fl(args); }
+void cmd_cat(char *args) { read_file(args); }
+
+void cmd_write(char *args) {
+char *space = strchr(args, ' ');
+if (space) {
+*space = '\0';
+write_file(args, space + 1);
+} else {
+puts("Usage: write <file> <text>");
+}
+}
+
+void cmd_del_line(char *args) {
+char *space = strchr(args, ' ');
+if (space) {
+*space = '\0';
+delete_line(args, atoi(space + 1));
+} else {
+puts("Usage: del_line <file> <line number>");
+}
+}
+
+void cmd_cr_proc(char *args) { cr_proc(args); }
+void cmd_dl_proc(char *args) { dl_proc(atoi(args)); }
+void cmd_ps(char *args) { list_processes(); }
+
+void cmd_profile_create(char *args) { profile_create(); }
+void cmd_profile_list(char *args) { profile_list(); }
+void cmd_profile_edit(char *args) { profile_edit(); }
+void cmd_profile_del(char *args) { profile_del(); }
+void cmd_login(char *args) { user_login(); }
+void cmd_logout(char *args) { logout(); }
+void cmd_usermode(char *args) { user_mode_prompt(); }
+
+void cmd_nicopat(char *args) { nicopat_show(); }
+
+void cmd_poweroff(char *args) {
+puts("Shutting down the system...");
+system("poweroff");
+}
+
+void cmd_reboot(char *args) {
+puts("Rebooting the system...");
+system("reboot");
+}
+
+void cmd_exit(char *args) {
+puts("Exiting shell...");
+exit(0);
+}
+
+void cmd_clear(char *args) {
+if (system("clear") == -1) perror("Error running clear");
+}
+
+void cmd_echo(char *args) {
+if (args && *args) printf("%s\n", args);
+else puts("Error: no text to echo.");
+}
+
+void cmd_gcc(char *args) { toolkit_gcc(args); }
+void cmd_python(char *args) { toolkit_python(args); }
+void cmd_nano(char *args) { toolkit_nano(args); }
+
+void cmd_exec(char *args) {
+char cmd_path[CMD_BUFFER];
+snprintf(cmd_path, sizeof(cmd_path), "UserFiles/%s", args);
+if (system(cmd_path) == -1) {
+printf("sh: %s: command not found\n", args);
+}
+}
+
+// ==== Command table ====
+Command commands[] = {
+// Files
+{"ls", cmd_ls, "Show file list"},
+{"touch", cmd_touch, "Create a file"},
+{"rm", cmd_rm, "Delete a file"},
+{"cat", cmd_cat, "Read a file"},
+{"write", cmd_write, "Write to a file"},
+{"del_line", cmd_del_line, "Delete a line from a file"},
+
+// Processes
+{"cr_proc", cmd_cr_proc, "Create a process"},
+{"dl_proc", cmd_dl_proc, "Delete a process"},
+{"ps", cmd_ps, "Show process list"},
+
+// Profiles
+{"profile_create", cmd_profile_create, "Create profile"},
+{"profile_list", cmd_profile_list, "List profiles"},
+{"profile_edit", cmd_profile_edit, "Edit profile"},
+{"profile_del", cmd_profile_del, "Delete profile"},
+{"UserLogin", cmd_login, "Login to profile"},
+{"Logout", cmd_logout, "Logout from profile"},
+{"UserMode", cmd_usermode, "Show current mode"},
+
+//System
+{"nicopat", cmd_nicopat, "Show system info"},
+{"poweroff", cmd_poweroff, "Shutdown system"},
+{"reboot", cmd_reboot, "Reboot system"},
+{"exit", cmd_exit, "Exit shell"},
+
+// General
+{"clear", cmd_clear, "Clear screen"},
+{"echo", cmd_echo, "Output text"},
+
+// External tools
+{"gcc", cmd_gcc, "Compile a file (UserFiles)"},
+{"python", cmd_python, "Run a Python script (UserFiles)"},
+{"nano", cmd_nano, "Edit a file (UserFiles)"},
+};
+
+// ==== Auto-generation help ====
+void cmd_help(char *args) {
+printf("\033[1;32mAvailable commands:\033[0m\n");
+size_t num_cmds = sizeof(commands) / sizeof(commands[0]);
+for (size_t i = 0; i < num_cmds; i++) {
+printf(" \033[1;34m%-12s\033[0m %s\n", commands[i].name, commands[i].description);
+}
+printf(" \033[1;34m./<file>\033[0m Execute file from UserFiles\n");
+}
+
+// ==== Run command ====
 void run_command(char *input) {
-    if (strcmp(input, "ls") == 0) {
-        system("ls UserFiles");
-    }
-    else if (strncmp(input, "touch ", 6) == 0) {
-        cr_fl(input + 6);
-    }
-    else if (strncmp(input, "rm ", 3) == 0) {
-        dl_fl(input + 3);
-    }
-    else if (strncmp(input, "cat ", 4) == 0) {
-        read_file(input + 4);
-    }
-    else if (strncmp(input, "write ", 6) == 0) {
-        char *space = strchr(input + 6, ' ');
-        if (space) {
-            *space = '\0';
-            write_file(input + 6, space + 1);
-        } else {
-            printf("Invalid format: write <file> <text>\n");
-        }
-    }
-    else if (strncmp(input, "del_line ", 9) == 0) {
-        char *space = strchr(input + 9, ' ');
-        if (space) {
-            *space = '\0';
-            int line_num = atoi(space + 1);
-            delete_line(input + 9, line_num);
-        } else {
-            printf("Invalid format: del_line <file> <line number>\n");
-        }
-    }
-    else if (strncmp(input, "cr_proc ", 8) == 0) {
-        cr_proc(input + 8);
-    }
-    else if (strncmp(input, "dl_proc ", 8) == 0) {
-        dl_proc(atoi(input + 8));
-    }
-    else if (strcmp(input, "ps") == 0) {
-        list_processes();
-    }
-    else if (strcmp(input, "profile_create") == 0) {
-        profile_create();
-    }
-    else if (strcmp(input, "profile_list") == 0) {
-        profile_list();
-    }
-    else if (strcmp(input, "profile_edit") == 0) {
-        profile_edit();
-    }
-    else if (strcmp(input, "profile_del") == 0) {
-        profile_del();
-    }
-    else if (strcmp(input, "UserLogin") == 0) {
-        user_login();
-    }
-    else if (strcmp(input, "Logout") == 0) {
-        logout();
-    }
-    else if (strcmp(input, "UserMode") == 0) {
-        user_mode_prompt();
-    }
-    else if (strcmp(input, "nicopat") == 0) {
-        nicopat_show();
-    }
-    else if (strcmp(input, "poweroff") == 0) {
-        printf("Shutting down the system...\n");
-        system("poweroff");
-    }
-    else if (strcmp(input, "reboot") == 0) {
-        printf("Rebooting the system...\n");
-        system("reboot");
-    }
-    else if (strcmp(input, "exit") == 0) {
-        printf("Exiting shell...\n");
-        exit(0);
-    }
-    else if (strcmp(input, "help") == 0) {
-        print_help();
-    }
-    else if (strcmp(input, "clear") == 0) {
-        system("clear");
-    }
-    else if (strncmp(input, "echo ", 5) == 0) {
-        printf("%s\n", input + 5);
-    }
-    else if (strncmp(input, "gcc ", 4) == 0) {
-        toolkit_gcc(input + 4);
-    }
-    else if (strncmp(input, "python ", 7) == 0) {
-        toolkit_python(input + 7);
-    }
-    else if (strncmp(input, "nano ", 5) == 0) {
-        toolkit_nano(input + 5);
-    }
-    else if (strncmp(input, "./", 2) == 0) {
-        char cmd[512];
-        snprintf(cmd, sizeof(cmd), "UserFiles/%s", input + 2);
-        int res = system(cmd);
-        if (res == -1) {
-            printf("sh: line 1: %s: command not found\n", input);
-        }
-    }
-    else {
-        printf("sh: line 1: %s: command not found\n", input);
-    }
+if (!input || !*input) return;
+
+// Executable files "./..."
+if (strncmp(input, "./", 2) == 0) {
+cmd_exec(input + 2);
+return;
 }
 
-void clear_screen() {
-    if (system("clear") == -1) {
-        perror("Error running clear command");
-    }
+// Separate the command and arguments
+char *args = strchr(input, ' ');
+if (args) {
+*args = '\0';
+args++;
+} else {
+args = "";
 }
 
-void print_help() {
-    printf("\033[1;32mAvailable commands:\033[0m\n");
-    printf("\n\033[1;34m--- Files ---\033[0m\n");
-    printf("ls\t\t\tShow file list\n");
-    printf("touch <file>\t\tCreate a file\n");
-    printf("rm <file>\t\tDelete a file\n");
-    printf("cat <file>\t\tRead a file\n");
-    printf("write <file> <text>\tWrite to a file\n");
-    printf("del_line <file> <#>\tDelete a line from a file\n");
-    printf("\n\033[1;34m--- Processes ---\033[0m\n");
-    printf("cr_proc <name>\t\tCreate a process\n");
-    printf("dl_proc <ID>\t\tDelete a process\n");
-    printf("ps\t\t\tShow process list\n");
-    printf("\n\033[1;34m--- Profiles ---\033[0m\n");
-    printf("profile_create\t\tCreate profile\n");
-    printf("profile_list\t\tList profiles\n");
-    printf("profile_edit\t\tEdit profile\n");
-    printf("profile_del\t\tDelete profile\n");
-    printf("UserLogin\t\tLogin to profile\n");
-    printf("Logout\t\t\tLogout from profile\n");
-    printf("UserMode\t\tShow current mode\n");
-    printf("\n\033[1;34m--- System ---\033[0m\n");
-    printf("nicopat\t\t\tShow system info\n");
-    printf("poweroff\t\tShutdown system\n");
-    printf("reboot\t\t\tReboot system\n");
-    printf("exit\t\t\tExit shell\n");
-    printf("\n\033[1;34m--- General ---\033[0m\n");
-    printf("help\t\t\tShow help\n");
-    printf("clear\t\t\tClear screen\n");
-    printf("echo <text>\t\tOutput text\n");
-    printf("\n\033[1;34m--- External tools ---\033[0m\n");
-    printf("gcc <src> <opts>\tCompile a file (UserFiles)\n");
-    printf("python <script>\t\tRun a Python script (UserFiles)\n");
-    printf("nano <file>\t\tEdit a file (UserFiles)\n");
+// help is separate so that it can work with the table
+if (strcmp(input, "help") == 0) {
+cmd_help(args);
+return;
 }
 
-void echo(char *text) {
-    if (text && *text) {
-        printf("%s\n", text);
-    } else {
-        printf("Error: no text to echo.\n");
-    }
-}
+// Search in the table
+size_t num_cmds = sizeof(commands) / sizeof(commands[0]);
+for (size_t i = 0; i < num_cmds; i++) {
+if (strcmp(input, commands[i].name) == 0) {
+commands[i].handler(args);
